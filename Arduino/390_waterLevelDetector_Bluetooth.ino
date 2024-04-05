7c1a7795e28d1c6a887e76ec4f6611b2626f4750
a-fratip <104953029+a-fratip@users.noreply.github.com>
2024-04-05 13:53:08 -0400

    Update 390_waterLevelDetector_Bluetooth.ino
    Added orientation detection with ADXL345 accelerometer

--- Arduino/390_waterLevelDetector_Bluetooth.ino
+++ Arduino/390_waterLevelDetector_Bluetooth.ino
@@ -1,9 +1,10 @@
  // US Sensor and LED pins
 #define PIN_TRIG 27
 #define PIN_ECHO 26
-#define LOW_LED 18
-#define MID_LED 19
-#define HIGH_LED 21
+#define GREEN_LED 19
+#define YELLOW_LED 18
+#define RED_LED 5
+#define BLUE_LED 32
 
 #define WH_MAX 16 // Define the maximum height of water (in cm) insdie the bottle
 #define WH_OFFSET 2 // Define the distance between sensor and 100% water level (ignored height)
@@ -21,7 +22,12 @@ const int WINDOW_SIZE = 7; // define window size for median filtering
 
 BluetoothSerial SerialBT; // bluetooth serial object
 
-// Sensor readings variables
+// Accelerometer variables
+int ADXL345 = 0x53; // The ADXL345 sensor I2C address
+float X_out, Y_out, Z_out; // Outputs
+int X_offset = 0, Y_offset = 0, Z_offset = 0; // Offset values
+
+// US Sensor readings variables
 long duration, distance;
 double waterHeight, waterLevel, bottleCapacity, waterConsumption;
 
@@ -39,25 +45,30 @@ void setup() {
   Serial.println("The device started, now you can pair it with bluetooth!");
 
   // Specify that LED pins will be used as output pins
-  pinMode(LOW_LED, OUTPUT);
-  pinMode(MID_LED, OUTPUT);
-  pinMode(HIGH_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
 
   // Set LED pins to low voltage level
-  digitalWrite(LOW_LED, LOW);
-  digitalWrite(MID_LED, LOW);
-  digitalWrite(HIGH_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(BLUE_LED, LOW);
 
   // Specify input/output pins of US sensor
   pinMode(PIN_TRIG, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
 
-  // Start a new measurement:
-  // Sensor is triggered by a HIGH pulse of 10 us or more
-  // Provide a short LOW pulse first to ensure a clean HIGH pulse
-  digitalWrite(PIN_TRIG, LOW);
+  // Setup ADXL345
+  Wire.begin();
+  configureADXL345(); // Configure the sensor
+  calibrateADXL345(); // Calibrate the sensor
+
+  // Start a new US sensor measurement:
+  digitalWrite(PIN_TRIG, LOW);  // Provide a short LOW pulse first to ensure a clean HIGH pulse
   delayMicroseconds(5);
-  digitalWrite(PIN_TRIG, HIGH);
+  digitalWrite(PIN_TRIG, HIGH);  // Sensor is triggered by a HIGH pulse of 10 us or more
   delayMicroseconds(10);
   digitalWrite(PIN_TRIG, LOW);
 
@@ -69,9 +80,10 @@ void setup() {
 }
 
 void loop() {
-  // Start a new measurement:
-  // Sensor is triggered by a HIGH pulse of 10 us or more
-  // Provide a short LOW pulse first to ensure a clean HIGH pulse
+
+  if (verticallyOriented()) {
+
+    // Start a new US sensor measurement:
   digitalWrite(PIN_TRIG, LOW);
   delayMicroseconds(5);
   digitalWrite(PIN_TRIG, HIGH);
@@ -106,30 +118,40 @@ void loop() {
   if (waterLevel <= 100 && waterLevel >= 50) // bottle is 50-100 % full
   {
     // turn on red LED
-    digitalWrite(LOW_LED, LOW);
-    digitalWrite(MID_LED, LOW);
-    digitalWrite(HIGH_LED, HIGH);
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(YELLOW_LED, LOW);
+    digitalWrite(BLUE_LED,LOW);
+    digitalWrite(RED_LED, HIGH);
     Serial.println("Keep hydrating! You've drank less than half of the water bottle!");
   }
   else if (waterLevel < 50 && waterLevel > 0) // bottle is less than 50% full, but not empty
   {
     // turn on yellow LED
-    digitalWrite(LOW_LED, LOW);
-    digitalWrite(MID_LED, HIGH);
-    digitalWrite(HIGH_LED, LOW);
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(YELLOW_LED, HIGH);
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(BLUE_LED,LOW);
     Serial.println("Keep hydrating. You've drank more than half of the water bottle!");
   }
   else if (waterHeight >= 0 && waterHeight <=1)
   {
     // turn on green LED
-    digitalWrite(LOW_LED, HIGH);
-    digitalWrite(MID_LED, LOW);
-    digitalWrite(HIGH_LED, LOW);
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(YELLOW_LED, LOW);
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(BLUE_LED,LOW);
     Serial.println("Congrats! You drank a full water bottle!");
     Serial.println("Please refill the water bottle");
   }
 
   delay(1000);
+
+
+  }
+  
+
+
+
 }
 
 void getDistanceReading() {
@@ -180,6 +202,119 @@ long medianFilter() {
 
   // calculate the median value
   return calculateMedianValue(sortedDistanceReadings);
+
+}
+
+void calibrateADXL345() {
+  // calibration with the positive x-axis pointed upwards against gravity
+ 
+  float numReadings = 500;
+  float xSum = 0;
+  // float ySum = 0;
+  // float zSum = 0;
+
+  // take 500 readings
+  for (int i = 0; i < numReadings; i++) {
+    Wire.beginTransmission(ADXL345);
+    Wire.write(0x32); // Start with register 0x32 (ACCEL_XOUT_H)
+    Wire.endTransmission(false);
+    Wire.requestFrom(ADXL345, 6, true);
+    // read raw values
+    X_out = (Wire.read() | Wire.read() << 8);
+    Y_out = (Wire.read() | Wire.read() << 8);
+    Z_out = (Wire.read() | Wire.read() << 8);
+    // add to running sum
+    xSum += X_out;
+    // ySum += Y_out;
+    // zSum += Z_out;
+  }
+  // 256 is the raw value for 1g: x-axis calibrated at 1g, y and z-axis at 0
+
+  // calculate offsets
+  X_offset = (256 - xSum / numReadings) / 4;
+
+  // Y_offset = (0-ySum / numReadings) / 4;
+
+  // Z_offset = (0-zSum / numReadings) / 4;
+ 
+  delay(1000);
+  
+  // Write offsets to respective offset registers
+  Wire.beginTransmission(ADXL345);
+  Wire.write(0x1E); // x-axis offset register
+  Wire.write(X_offset);
+  Wire.endTransmission();
+
+  // Wire.beginTransmission(ADXL345);
+  // Wire.write(0x1F); // y-axis offset register
+  // Wire.write(Y_offset);
+  // Wire.endTransmission();
+
+  // Wire.beginTransmission(ADXL345);
+  // Wire.write(0x20); // z-axis offset register
+  // Wire.write(Z_offset); // 
+  // Wire.endTransmission();
+}
+
+void configureADXL345() {
+  // Wire.beginTransmission(ADXL345);
+  // Wire.write(0x2C); // BW_RATE register
+  // Wire.write(0x08); // set data rate (D3 bit high)
+  // Wire.endTransmission();
+
+  // Wire.beginTransmission(ADXL345);
+  // Wire.write(0x31); // DATA_FORMAT register
+  // Wire.write(0x08); // Enable FULL_RES (D3 bit high)
+  // Wire.endTransmission();
+
+  Wire.beginTransmission(ADXL345);
+  Wire.write(0x2D); // POWER_CTL Register
+  Wire.write(8); // Enable measurement (D3 bit high)
+  Wire.endTransmission();
+
+
+  delay(10);
+}
+
+void readAccelData() {
+  // Read accelerometer data
+  Wire.beginTransmission(ADXL345);
+  Wire.write(0x32); // Start with register 0x32 (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(ADXL345, 6, true); // Read 6 registers total, each axis value stored in 2 registers
+  X_out = (Wire.read() | Wire.read() << 8); // Raw x-axis value
+  X_out = X_out / 256; // For default sensitivity (+-2g), divide by 256 
+  Y_out = (Wire.read() | Wire.read() << 8); // Raw y-axis value
+  Y_out = Y_out / 256;
+  Z_out = (Wire.read() | Wire.read() << 8); // Raw z-axis value
+  Z_out = Z_out / 256;
+}
+
+bool verticallyOriented() {
+  // Averaging of ADXL345
+  float averageX_out = 0;
+  float numReadings = 16;
+  for (int i = 0; i < numReadings; i++) {
+    readAccelData();
+    averageX_out += X_out;
+    delay(1);
+  }
+  averageX_out /= numReadings;
+
+  Serial.print("Average X_out = ");
+  Serial.println(averageX_out);
+  // delay(1000);
+
+  if (averageX_out <= 1.1 && averageX_out >= 0.98) {
+    return true;
+  }
+
+  // if not vertically oriented
+  digitalWrite(BLUE_LED,HIGH);
+  digitalWrite(GREEN_LED, LOW);
+  digitalWrite(YELLOW_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+  return false;
 
 }
 
