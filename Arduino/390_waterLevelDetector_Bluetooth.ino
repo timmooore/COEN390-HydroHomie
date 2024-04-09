d5e5df364eabcfbad1d2d8081c2f6327cb89208f
a-fratip <104953029+a-fratip@users.noreply.github.com>
2024-04-09 15:37:06 -0400

    Update 390_waterLevelDetector_Bluetooth.ino
    Updated water level measurement for sensing bottle refill

--- Arduino/390_waterLevelDetector_Bluetooth.ino
+++ Arduino/390_waterLevelDetector_Bluetooth.ino
@@ -3,8 +3,9 @@
 #define PIN_ECHO 26
 #define GREEN_LED 19
 #define YELLOW_LED 18
-#define RED_LED 5
+#define RED_LED 4
 #define BLUE_LED 32
+#define ADXL345_POWER_PIN 17
 
 #define WH_MAX 16 // Define the maximum height of water (in cm) insdie the bottle
 #define WH_OFFSET 2 // Define the distance between sensor and 100% water level (ignored height)
@@ -22,6 +23,8 @@ const int WINDOW_SIZE = 7; // define window size for median filtering
 
 BluetoothSerial SerialBT; // bluetooth serial object
 
+char cmd; // variable for reading from bluetooth serial terminal
+
 // Accelerometer variables
 int ADXL345 = 0x53; // The ADXL345 sensor I2C address
 float X_out, Y_out, Z_out; // Outputs
@@ -29,14 +32,22 @@ int X_offset = 0, Y_offset = 0, Z_offset = 0; // Offset values
 
 // US Sensor readings variables
 long duration, distance;
-double waterHeight, waterLevel, bottleCapacity, waterConsumption;
+double currWaterHeight, waterConsumption;
+double prevWaterHeight = 0;
+
+// Water level activity flags
+bool waterConsumed = false;
+bool waterRefilled = false;
+bool idle = false;
 
 long distanceReadings[WINDOW_SIZE]; // Array to store sensor readings
-size_t static idx = 0; // index to keep track of current position in the array
+size_t static idx = 0; // index to keep track of current position in the array ranging from 0 to (WINDOW_SIZE - 1)
 
 // Timer: auxiliar variables
 unsigned long previousMillis = 0;    // Stores last time reading was published
 const long interval = 1000;         // interval at which to publish sensor readings
+
+/****************************************************************************************************************************************************************************************************/
 
 void setup() {
   Serial.begin(9600); // Initialize serial communication with a baud rate of 9600 bits per second
@@ -60,22 +71,20 @@ void setup() {
   pinMode(PIN_TRIG, OUTPUT);
   pinMode(PIN_ECHO, INPUT);
 
+  // Specify pin connected to VCC of ADXL345
+  pinMode(ADXL345_POWER_PIN, OUTPUT);
+  digitalWrite(ADXL345_POWER_PIN, HIGH);
+
   // Setup ADXL345
   Wire.begin();
   configureADXL345(); // Configure the sensor
   calibrateADXL345(); // Calibrate the sensor
 
-  // Start a new US sensor measurement:
-  digitalWrite(PIN_TRIG, LOW);  // Provide a short LOW pulse first to ensure a clean HIGH pulse
-  delayMicroseconds(5);
-  digitalWrite(PIN_TRIG, HIGH);  // Sensor is triggered by a HIGH pulse of 10 us or more
-  delayMicroseconds(10);
-  digitalWrite(PIN_TRIG, LOW);
-
-  // get initial distance readings to populate the array
-  while ( (sizeof(distanceReadings)) / (sizeof(distanceReadings[0])) != WINDOW_SIZE) {
-    getDistanceReading();
-  }
+  // configSensor(); // start a new US sensor measurement
+  // // get initial distance readings to populate the array
+  // while ( (sizeof(distanceReadings)) / (sizeof(distanceReadings[0])) != WINDOW_SIZE) {
+  //   getDistanceReading();
+  // }
 
 }
 
@@ -83,84 +92,160 @@ void loop() {
 
   if (verticallyOriented()) {
 
-    // Start a new US sensor measurement:
-  digitalWrite(PIN_TRIG, LOW);
-  delayMicroseconds(5);
-  digitalWrite(PIN_TRIG, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(PIN_TRIG, LOW);
+    calculateWaterLevel();
 
-  getDistanceReading();
+    if (waterConsumed) {
+      Serial.print("You have consumed: " + String(waterConsumption) + "mL\n");
+    
+      // Send amount of water consumed to bluetooth serial terminal
+      SerialBT.println("You have consumed: " + String(waterConsumption) + "mL\n");
 
-  long medianDistance = medianFilter();
+    }
+   
+
   
-  // get water level measurements
-  waterHeight = (WH_MAX+WH_OFFSET) - medianDistance; // get the water height in cm
-  waterLevel = waterHeight / WH_MAX * 100; // percentage of water height in the bottle
-  bottleCapacity = waterLevel/100 * WC_TOT; // Real-time capacity of the bottle
-  waterConsumption = WC_TOT - bottleCapacity;
+  // get water level measurements (old)
+  // waterHeight = (WH_MAX+WH_OFFSET) - medianDistance; // get the water height in cm
+  // waterLevel = waterHeight / WH_MAX * 100; // percentage of water height in the bottle
+  // bottleCapacity = waterLevel/100 * WC_TOT; // Real-time capacity of the bottle
+  // waterConsumption = WC_TOT - bottleCapacity;
 
-  // Display the results
-  Serial.print("Distance: " + String(distance) + " cm\n");
-  Serial.print("Height of water: " + String(waterHeight) + " cm\n");
-  Serial.print("Water level percentage: " + String(waterLevel) + "%\n");
-  Serial.print("Amount of water in bottle: " + String(bottleCapacity) + " mL\n");
-  Serial.print("You have consumed : " + String(waterConsumption) + "mL\n");
+    // Display the results
+    Serial.print("Previous height of water: " + String(prevWaterHeight) + " cm" + ", Current height of water: " + String(currWaterHeight) + " cm\n");
+ 
 
   // Send readings to bluetooth serial terminal every second
-  unsigned long currentMillis = millis(); 
-  if (currentMillis - previousMillis >= interval){
-    previousMillis = currentMillis;
-    SerialBT.println("You have consumed : " + String(waterConsumption) + "mL");
+  // unsigned long currentMillis = millis(); 
+  // if (currentMillis - previousMillis >= interval){
+  //   previousMillis = currentMillis;
+  //   SerialBT.println("You have consumed : " + String(waterConsumption) + "mL");
+  // }
+
+    // TODO: Update conditions for lighting LEDs
+    if (currWaterHeight <= WH_MAX && currWaterHeight >= (WH_MAX/2)) // bottle is 50-100 % full
+    {
+      // turn on red LED
+      digitalWrite(GREEN_LED, LOW);
+      digitalWrite(YELLOW_LED, LOW);
+      digitalWrite(BLUE_LED,LOW);
+      digitalWrite(RED_LED, HIGH);
+      Serial.println("Keep hydrating! You've drank less than half of the water bottle!");
+    }
+    else if (currWaterHeight < (WH_MAX/2) && currWaterHeight > 1) // bottle is less than 50% full, but not empty
+    {
+      // turn on yellow LED
+      digitalWrite(GREEN_LED, LOW);
+      digitalWrite(YELLOW_LED, HIGH);
+      digitalWrite(RED_LED, LOW);
+      digitalWrite(BLUE_LED,LOW);
+      Serial.println("Keep hydrating. You've drank more than half of the water bottle!");
+    }
+    else if (currWaterHeight >= 0 && currWaterHeight <=1)
+    {
+      // turn on green LED
+      digitalWrite(GREEN_LED, HIGH);
+      digitalWrite(YELLOW_LED, LOW);
+      digitalWrite(RED_LED, LOW);
+      digitalWrite(BLUE_LED,LOW);
+      Serial.println("Congrats! You drank a full water bottle!");
+      Serial.println("Please refill the water bottle");
+    }
+
+    delay(1000);
+
+  } // end if vertically oriented
+  else {
+    Serial.println("Not vertically oriented...");
   }
 
-  // Set conditions for lighting LEDs
-  if (waterLevel <= 100 && waterLevel >= 50) // bottle is 50-100 % full
-  {
-    // turn on red LED
-    digitalWrite(GREEN_LED, LOW);
-    digitalWrite(YELLOW_LED, LOW);
-    digitalWrite(BLUE_LED,LOW);
-    digitalWrite(RED_LED, HIGH);
-    Serial.println("Keep hydrating! You've drank less than half of the water bottle!");
-  }
-  else if (waterLevel < 50 && waterLevel > 0) // bottle is less than 50% full, but not empty
-  {
-    // turn on yellow LED
-    digitalWrite(GREEN_LED, LOW);
-    digitalWrite(YELLOW_LED, HIGH);
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(BLUE_LED,LOW);
-    Serial.println("Keep hydrating. You've drank more than half of the water bottle!");
-  }
-  else if (waterHeight >= 0 && waterHeight <=1)
-  {
-    // turn on green LED
-    digitalWrite(GREEN_LED, HIGH);
-    digitalWrite(YELLOW_LED, LOW);
-    digitalWrite(RED_LED, LOW);
-    digitalWrite(BLUE_LED,LOW);
-    Serial.println("Congrats! You drank a full water bottle!");
-    Serial.println("Please refill the water bottle");
-  }
+} // end loop
 
-  delay(1000);
+/****************************************************************************************************************************************************************************************************/
 
+/* US Sensor Helper Functions */
 
-  }
-  
-
-
-
+void configSensor() {
+  // Start a new US sensor measurement:
+  digitalWrite(PIN_TRIG, LOW);  // Provide a short LOW pulse first to ensure a clean HIGH pulse
+  delayMicroseconds(5);
+  digitalWrite(PIN_TRIG, HIGH);  // Sensor is triggered by a HIGH pulse of 10 us or more
+  delayMicroseconds(10);
+  digitalWrite(PIN_TRIG, LOW);
 }
 
 void getDistanceReading() {
+
+  configSensor();
+
   // Read the result from sensor
   duration = pulseIn(PIN_ECHO, HIGH);
   distance = (duration/2) / 29.1; // convert time into a distance
 
   distanceReadings[idx] = distance; // add distance reading to array
   idx = (idx+1) % WINDOW_SIZE; // update array index and wrap back to zero if necessary
+
+  // Serial.print("Distance: " + String(distance) + " cm, idx: " + String(idx) + "\n");
+}
+
+void calculateWaterLevel() {
+  int numValidReadings = 0;
+
+  // take required number of valid readings
+  while (numValidReadings < WINDOW_SIZE) {
+    Serial.println("Measuring water level...");
+
+    getDistanceReading(); // add distance reading to distanceReadings array
+    
+    // Check if reading is in the acceptable range
+    if (idx != 0) { // array index is non-zero
+      if (distanceReadings[idx-1] <= (WH_MAX + WH_OFFSET) && distanceReadings[idx-1] >= WH_OFFSET) { // latest reading is stored at array index (idx-1)
+        numValidReadings++; // update the count of valid readings
+      }
+    }
+    else { // array index is zero
+      if (distanceReadings[WINDOW_SIZE - 1] <= (WH_MAX + WH_OFFSET) && distanceReadings[WINDOW_SIZE - 1] >= WH_OFFSET) { // latest reading is stored at array index (WINDOW_SIZE - 1)
+        numValidReadings++; // update the count of valid readings
+      }
+    }
+
+    delay(10);
+  }
+
+  Serial.println("Water level measured...");
+
+  long medianDistance = medianFilter(); // get the filtered distance reading
+  currWaterHeight = (WH_MAX+WH_OFFSET) - medianDistance; // get the water height in cm
+
+  if (currWaterHeight < prevWaterHeight) { // water is consumed from the bottle
+    waterConsumption = calculateWaterConsumption(prevWaterHeight, currWaterHeight); // get amount of water consumed (in mL)
+    prevWaterHeight = currWaterHeight;
+
+    // update water activity flags 
+    waterConsumed = true;
+    waterRefilled = false;
+    idle = false;
+  }
+  else if (currWaterHeight > prevWaterHeight) { // water is refilled
+    prevWaterHeight = currWaterHeight;
+
+    // update water activity flags 
+    waterConsumed = false;
+    waterRefilled = true;
+    idle = false;
+
+  }
+  else if (currWaterHeight == prevWaterHeight) { // no change in water level
+    // update water activity flags 
+    waterConsumed = false;
+    waterRefilled = false;
+    idle = true;
+  }
+
+}
+
+double calculateWaterConsumption(double prev, double curr) {
+  double diff = prev - curr; // get the difference in water height
+  return diff/WH_MAX * WC_TOT; // convert height (in cm) to amount in (mL)
 }
 
 void insertionSort(long arr[], int n) {
@@ -204,6 +289,8 @@ long medianFilter() {
   return calculateMedianValue(sortedDistanceReadings);
 
 }
+
+/* Accelerometer helper functions */
 
 void calibrateADXL345() {
   // calibration with the positive x-axis pointed upwards against gravity
@@ -309,12 +396,47 @@ bool verticallyOriented() {
     return true;
   }
 
-  // if not vertically oriented
+  // if not vertically oriented, turn on blue LED
   digitalWrite(BLUE_LED,HIGH);
   digitalWrite(GREEN_LED, LOW);
   digitalWrite(YELLOW_LED, LOW);
   digitalWrite(RED_LED, LOW);
   return false;
+
+}
+
+/* LED helper functions */
+
+void updateLEDs() {
+  // Read command from bluetooth serial terminal
+  if (SerialBT.available()){
+    cmd = SerialBT.read();
+  }
+
+  if (cmd == 'r') {
+    // turn on red LED
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(YELLOW_LED, LOW);
+    digitalWrite(BLUE_LED,LOW);
+    digitalWrite(RED_LED, HIGH);
+    Serial.println("Keep hydrating! You have not yet reached half of your hydration goal!");
+  }
+  else if (cmd == 'y') {
+    // turn on yellow LED
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(YELLOW_LED, HIGH);
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(BLUE_LED,LOW);
+    Serial.println("Keep hydrating. You've drank more than half of the water bottle!");
+  }
+  else if (cmd == 'g') {
+    // turn on green LED
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(YELLOW_LED, LOW);
+    digitalWrite(RED_LED, LOW);
+    digitalWrite(BLUE_LED,LOW);
+    Serial.println("Congrats! You have met your hydration goal");
+  }
 
 }
 
